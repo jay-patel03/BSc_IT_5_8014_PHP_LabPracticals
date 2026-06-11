b47270b6d5c19c247445d80f29f89c082ba6cdab
jay-patel03 <jaytarapada15@gmail.com>
2026-06-11 11:10:41 +0530

    Add files via upload

---
+++ p9_2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        error_reporting(0);
+    ?>
+    <form action="" method="get">
+        <input type="text" name="uname[0]"> <br> <br>
+        <input type="text" name="uname[1]"> <br> <br>
+        <input type="text" name="uname[2]"> <br> <br>
+
+        <input type="submit" value="add"> <br> <br>
+    </form>
+</body>
+</html>
+
+    <?php
+    $nm = $_GET['uname'];
+    echo"<h3> orignal array:</h3>";
+    foreach($nm as $val)
+    {
+        echo $val . "<br>";
+    }
+
+    echo"<h3> reverse array:</h3>";
+    $rev = array_reverse($nm);
+    foreach($rev as $val){
+        echo $val . "<br>";
+    }
+    ?>
