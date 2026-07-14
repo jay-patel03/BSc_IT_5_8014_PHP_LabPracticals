db3f83d517039eca82f6130b26950679a7eabf6c
jay-patel03 <jaytarapada15@gmail.com>
2026-07-14 09:14:36 +0530

    Add files via upload

---
+++ u2-p8_1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $conn = new mysqli("localhost:3307","root","","test");
+        if($conn->connect_error){
+            die("Connection failed:" .$conn->connect_error);
+        }
+        $queries = [
+            "SELECT DAYOFWEEK(CURDATE()) AS Dayofweek",
+            "SELECT WEEKDAY(CURDATE()) AS WeekDay",
+            "SELECT DAYOFMONTH(CURDATE()) AS DayofMonth",
+            "SELECT DAYOFYEAR(CURDATE()) AS DayofYear",
+            "SELECT DAYNAME(CURDATE()) AS DayName",
+            "SELECT MONTH(CURDATE()) AS MonthNumber,
+                    MONTHNAME(CURDATE()) AS MonthName",
+            "SELECT WEEK(CURDATE()) AS WeekNumber,
+                    NOW() AS CurrentDateTime",
+            "SELECT SYSDATE() AS SystemDateTime,
+                    CURRENT_TIMESTAMP() AS CurrentTimesTamp",
+        ];
+
+        foreach($queries as $sql){
+            echo"<br>";
+            $result = $conn->query($sql);
+            if($result){
+                while($row=$result->fetch_assoc()){
+                    foreach($row as $key=>$value)
+                    {
+                        echo"<b>$key :</b>";
+                    }
+                }
+            }
+            else{
+                echo "Query Error";
+            }
+    }
+    ?>
+</body>
+</html>
